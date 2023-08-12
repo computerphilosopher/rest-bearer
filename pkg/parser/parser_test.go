1900cd3c5b893b0e3dd6547d5e2773563f59a15b
skynet <bspark@whatap.io>
2023-08-13 01:02:07 +0900

    :recycle: Rename struct

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -7,13 +7,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestVulnerabilityParser(t *testing.T) {
+func TestMetaInfoParser(t *testing.T) {
 	assert := assert.New(t)
 	line := "MEDIUM: Insufficiently random value detected. [CWE-330]"
-	vulnerability, err := parser.ParseVulnerability(line)
+	metaInfo, err := parser.ParseMetaInfo(line)
 
 	assert.Nil(err)
-	assert.Equal(parser.Medium, vulnerability.Severity)
-	assert.Equal("CWE-330", vulnerability.Code)
-	assert.Equal("Insufficiently random value detected.", vulnerability.Description)
+	assert.Equal(parser.Medium, metaInfo.Severity)
+	assert.Equal("CWE-330", metaInfo.Code)
+	assert.Equal("Insufficiently random value detected.", metaInfo.Description)
 }
